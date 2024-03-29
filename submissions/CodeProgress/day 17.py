7bfbd46ec14a3d50638f46625ee7fa0973572944
Raiii <102576388+railley02@users.noreply.github.com>
2024-03-29 20:13:09 +0800

    Delete submissions/CodeProgress/day 17.py

--- submissions/CodeProgress/day 17.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get('https://jsonplaceholder.typicode.com/users')
-
-if response.status_code == 200:
-    print("Request successful.")
-    print(response.json())
-else:
-    print("Request failed.")
